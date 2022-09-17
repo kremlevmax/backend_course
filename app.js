05cd77ba985b3075ed3d2d176bd207c5dfc0c280
kremlevmax <kremlevmax1989@gmail.com>
2022-09-16 23:34:23 -0700

    Next chapter

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-// console.log(__dirname);
-// console.log(path.join("subfolder"));
-// console.log(path.resolve("subfolder"));
+console.log(__dirname);
+console.log(path.join("subfolder"));
+console.log(path.resolve("subfolder"));
 
 fs.readFile("./input.txt", "utf8", (err, result) => {
   if (err) {
