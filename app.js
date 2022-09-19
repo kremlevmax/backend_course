1132c7464da43eb13bf9eb527019e442c7c4923a
kremlevmax <kremlevmax1989@gmail.com>
2022-09-18 22:38:28 -0700

    Add Express

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+
+const path = require("path");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
+console.log(__dirname);
+console.log(path.join("subfolder"));
+console.log(path.resolve("subfolder"));
+console.log(path.resolve(__dirname, "subfolder"));
+
+app.get("/", (req, res) => {
   const fileStream = fs.createReadStream("./input.txt", "utf8");
 
   fileStream.on("open", () => {
@@ -9,6 +17,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
